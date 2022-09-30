6436ac0b261a1ee31bd6e542ce885bd0f629a62a
Karl <karl.lu@outlook.com>
2022-09-30 11:40:26 -0400

    added documentation to the basic JS for clarity

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,22 @@
+// initializes the app opject
 const drinkApp = {};
 
+// identifies form & dropdown items to track submission
 const drinkChoice = document.getElementById('alcoholType');
 const form = document.querySelector('form');
 
+// API URL and extensions used to find 
 drinkApp.URL = 'https://www.thecocktaildb.com/api/json/v1/1/';
 drinkApp.findDrink = 'filter.php'
 drinkApp.drinkDetails = 'lookup.php'
 
+// accepts an array, checks length, and returns a random number within range
 function randNum(array) {
-    const num = Math.floor(Math.random() * array.drinks.length);
+    const num = Math.floor(Math.random() * array.length);
     return num;
 }
 
+// function that uses URL & search to take user input (alcohol type) to return a random drink ID from list containing that alcohol, before passing to another function
 drinkApp.grabData = () => {
     const appURL = new URL(`${drinkApp.URL}${drinkApp.findDrink}`);
     appURL.search = new URLSearchParams({
@@ -22,12 +27,13 @@ drinkApp.grabData = () => {
             return promise.json()
         })
         .then((data) => {
-            const recDrink = (data.drinks[randNum(data)].idDrink)
+            const recDrink = (data.drinks[randNum(data.drinks)].idDrink)
             console.log(recDrink)
             drinkApp.grabDetails(recDrink)
         })
 }
 
+// function uses ID provided from grabData to obtain further information about drink (glass type, ingredients, instructions)
 drinkApp.grabDetails = (id) => {
     const appURL = new URL(`${drinkApp.URL}${drinkApp.drinkDetails}`);
     appURL.search = new URLSearchParams({
@@ -38,17 +44,22 @@ drinkApp.grabDetails = (id) => {
         return promise.json()
     })
     .then((data) => {
+        console.log(`Obtained information for ${data.drinks[0].strDrink}`)
         console.log(data)
     })
 }
 
+// app init creates form event listener for submission
 drinkApp.init = () => {
     form.addEventListener('submit', function (e) {
+        // prevents page reload
         e.preventDefault();
+        // saves user choice from dropdown list
         drinkApp.drink = drinkChoice.value;
+        // calls function to grab information regarding drink
         drinkApp.grabData();
     })
-
 }
 
+// calls init method on page initialization
 drinkApp.init();
