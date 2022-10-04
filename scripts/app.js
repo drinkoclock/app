1eef94e4b3ce31d2285de318136c7ea6d098d1f5
Karl <karl.lu@outlook.com>
2022-10-04 00:55:18 -0400

    added JS logic to use extracted API info to parse information and insert into HTML

--- scripts/app.js
+++ scripts/app.js
@@ -4,11 +4,11 @@ const drinkApp = {};
 // identifies form & dropdown items to track submission
 const drinkChoice = document.getElementById('alcoholType');
 const form = document.querySelector('form');
-const drinkInstructions = document.getElementById('drinkInstructions');
-const drinkIngredients = document.getElementById('drinkIngredients');
+drinkApp.instructions = document.getElementById('drinkInstructions');
+drinkApp.ingredients = document.getElementById('drinkIngredients');
 
 // API URL and extensions used to find 
-drinkApp.URL = 'https://www.thecocktaildb.com/api/json/v1/1/';
+drinkApp.baseUrl = 'https://www.thecocktaildb.com/api/json/v1/1/';
 drinkApp.findDrink = 'filter.php';
 drinkApp.drinkDetails = 'lookup.php';
 drinkApp.randomDrink = 'random.php';
@@ -20,36 +20,110 @@ function randNum(array) {
 }
 
 // function that uses URL & search to take user input (alcohol type) to return a random drink ID from list containing that alcohol, before passing to another function
-drinkApp.grabData = () => {
-    const appURL = new URL(`${drinkApp.URL}${drinkApp.findDrink}`);
-    appURL.search = new URLSearchParams({
+
+drinkApp.selectDrink = () => {
+    const url = new URL(drinkApp.baseUrl + drinkApp.findDrink);
+    url.search = new URLSearchParams({
         i: drinkApp.drink
     })
-    fetch(appURL)
+    fetch(url)
         .then((promise) => {
             return promise.json()
         })
         .then((data) => {
             const recDrink = (data.drinks[randNum(data.drinks)].idDrink)
-            console.log(recDrink)
-            drinkApp.grabDetails(recDrink)
+            drinkApp.getDrinkDetails(recDrink)
+        })
+        .catch((e) => {
+            console.log("error: ", e)
         })
 }
 
 // function uses ID provided from grabData to obtain further information about drink (glass type, ingredients, instructions)
-drinkApp.grabDetails = (id) => {
-    const appURL = new URL(`${drinkApp.URL}${drinkApp.drinkDetails}`);
-    appURL.search = new URLSearchParams({
+drinkApp.getDrinkDetails = (id) => {
+    const url = new URL(drinkApp.baseUrl + drinkApp.drinkDetails);
+    url.search = new URLSearchParams({
         i: id
     })
-    fetch(appURL)
-    .then((promise) => {
-        return promise.json()
-    })
-    .then((data) => {
-        console.log(`Obtained information for ${data.drinks[0].strDrink}`)
-        console.log(data)
-    })
+    fetch(url)
+        .then((promise) => {
+            return promise.json()
+        })
+        .then((data) => {
+            console.log(`Obtained information for ${data.drinks[0].strDrink}`, data)
+            drinkApp.recDrink = data.drinks[0];
+            drinkApp.populateInstructions(drinkApp.recDrink.strInstructions);
+            drinkApp.populateIngredients(drinkApp.recDrink);
+        })
+        .catch((e) => {
+            console.log("error: ", e)
+        })
+}
+
+// this inserts the instructions from the API into the HTML element
+drinkApp.populateInstructions = (inst) => {
+    // clears the innerHTML and opens an ordered list
+    drinkApp.instructions.innerHTML = "<ol>";
+    // checks for end of instructions to ensure there's a period to avoid missing the final instruction
+   (inst[inst.length - 1] !== "." ? inst+="." : "")
+    // will continue loop until all periods (end of sentences are removed)
+    while (inst.indexOf('.') !== -1) {
+        // this extracts the first four characters of the string
+        const checkForStep = inst.slice(0, 4).toLowerCase()
+
+        // series of checks to ensure proper formatting
+        // first checks if the first four characters are 'step', if so, removes the first 6 characters (ie. step x)
+        if (checkForStep === "step") {
+            const restInst = inst.slice(6)
+            inst = restInst.trim();
+        }
+        // checks to see if instructions uses numeric listing already, and removes the number and period
+        else if (inst.indexOf(".").isInteger) {
+            alert('catch');
+        }
+        // this catches elipses at the end of certain instructions
+        else if (inst.indexOf(".") === 0) {
+            const restInst = inst.slice(inst.indexOf(".") + 1)
+            inst = restInst.trim();
+        }
+        // this outputs the instruction step
+        else {
+            const nextInst = inst.slice(0, (inst.indexOf(".") + 1))
+            const restInst = inst.slice(inst.indexOf(".") + 1)
+            inst = restInst.trim();
+            drinkApp.instructions.firstElementChild.innerHTML += `
+                <li>${nextInst}</li>
+            `
+        }
+    }
+    // closes the ordered list
+    drinkApp.instructions.innerHTML += "</ol>"
+}
+
+// inserts ingredient list & measurements from API into the HTML element
+drinkApp.populateIngredients = (drink) => {
+    // clears ingredients element and inserts table
+    drinkApp.ingredients.innerHTML = "<table><tbody>"
+    // creates a for loop to go through a maximum possible 15 ingredients and list them
+    for (i = 1; i < 16; i++) {
+        // creates string literals to match the keys for both ingredients and measurements when searching
+        const ingVar = `strIngredient${i}`;
+        const ingMeasVar = `strMeasure${i}`;
+
+        // checks to see if the current ingredient exists, then inserts them into the table if they are
+        if (drink[ingVar]) {
+            drinkApp.ingredients.firstElementChild.innerHTML += `
+            <tr>
+                <td>${drink[ingVar]}</td>
+                <td>${drink[ingMeasVar]}</td>
+            </tr>
+            `
+        } 
+        // if not, breaks the loop to avoid needless iterations
+        else break;
+    }
+    // when for loop is complete, closes table
+    drinkApp.ingredients.firstElementChild.innherHTML += "</tbody></table";
 }
 
 // app init creates form event listener for submission
@@ -60,13 +134,13 @@ drinkApp.init = () => {
         // saves user choice from dropdown list
         drinkApp.drink = drinkChoice.value;
         console.log(drinkApp.drink);
-       
+
         // checks to see if user has made a selection before making fetch request
         if (drinkApp.drink) {
-            drinkApp.grabData();
+            drinkApp.selectDrink();
         } else {
             alert('pick something');
-        }        
+        }
     })
 }
 
